932ee547bc2a35c57911ac9e0d46c78ee5bb6ef7
Gean Carlos Farias <gcar95@hotmail.com>
2019-07-15 17:46:41 -0300

    Teste iniciais com movimentos possiveis das pecas

--- Tela.cs
+++ Tela.cs
@@ -15,19 +15,42 @@ namespace xadrez_console
                 Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
-                    if (tab.peca(i, j) == null)
-                    {
-                        Console.Write("- ");
-                    }
-                    else
-                    {
-                        imprimirPeca(tab.peca(i, j));
-                        Console.Write(" ");
-                    }
+                    imprimirPeca(tab.peca(i, j));
+                    Console.Write(" ");
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("  a b c d e f g h");
+            Console.WriteLine("  a  b  c  d  e  f  g  h");
+        }
+
+
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis)
+        {
+            ConsoleColor fundooriginal = Console.BackgroundColor;
+            ConsoleColor fungoalterado = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (posicoesPossiveis[i, j])
+                    {
+                        Console.BackgroundColor = fungoalterado;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = fundooriginal;
+                    }
+                    imprimirPeca(tab.peca(i, j));
+                    Console.Write(" ");
+                    Console.BackgroundColor = fundooriginal;
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("  a  b  c  d  e  f  g  h");
+            Console.BackgroundColor = fundooriginal;
         }
 
         public static PosicaoXadrez lerPosicaoXadrez()
@@ -40,16 +63,24 @@ namespace xadrez_console
 
         public static void imprimirPeca(Peca peca)
         {
-            if (peca.cor == Cor.Branca)
+            if (peca == null)
             {
-                Console.Write(peca);
+                Console.Write("- ");
             }
             else
             {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.Write(peca);
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
             }
         }
 
